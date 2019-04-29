c0e05be7f27c8afd87e24c46215b105a6b73ed49
Highnewb <thouveninvalentin@gmail.com>
2019-04-29 22:36:24 +0200

    Ajouts sur BDD

---
+++ src/dao/ConnexionDB.java
@@ -0,0 +1,54 @@
+package dao;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnexionDB {
+
+	private static volatile ConnexionDB instance;
+	private Connection cnx; 
+	
+	private ConnexionDB() {
+		try {
+			Properties p = new Properties();
+			p.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("confDB.properties"));
+			
+			
+				
+			// chargement du driver
+			Class.forName(p.getProperty("driver"));
+			cnx = DriverManager.getConnection(p.getProperty("url"),
+					p.getProperty("user"), p.getProperty("pwd"));		
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	} 
+	
+	public static synchronized ConnexionDB getInstance() {
+		if(instance==null)
+			instance = new ConnexionDB();
+		
+		return instance;
+	}
+
+	public Connection getCnx() {
+		return cnx;
+	}
+
+	public void closeCnx() throws SQLException{
+		if(cnx!=null){
+			cnx.close();
+			instance=null;
+		}
+	}
+	
+}
---
+++ src/dao/UsersDAO.java
@@ -0,0 +1,51 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UsersDAO {
+
+private Connection co;
+	
+	public UsersDAO() {
+		co = ConnexionDB.getInstance().getCnx();
+	}
+	
+	public void insertUser(String pseudo, String password) throws SQLException {
+		PreparedStatement stmt = co.prepareStatement("INSERT INTO users (pseudo,password,role) values (?,?,?);");
+		stmt.setString(1, pseudo);
+		stmt.setString(2, password);
+		stmt.setString(3, "user");
+		stmt.executeUpdate();
+	}
+	
+	public ResultSet getUsers() throws SQLException {
+		PreparedStatement stmt = co.prepareStatement("SELECT * FROM users");
+		return stmt.executeQuery(); 		
+	}
+	
+	public void deleteUser(int id) throws SQLException {
+		PreparedStatement stmt = co.prepareStatement("DELETE FROM users WHERE id = ?;");
+		stmt.setInt(1, id);
+		stmt.executeUpdate();
+	}
+	
+	public void modifyUser(int id, String pseudo, String password) throws SQLException {
+		PreparedStatement stmt = co.prepareStatement("UPDATE users SET pseudo = ?, password = ? WHERE id = ?");
+		stmt.setString(1, pseudo);
+		stmt.setString(2, password);
+		stmt.setInt(3, id);
+		stmt.executeUpdate();
+	}
+	
+	public ResultSet getUser(String pseudo, String password) throws SQLException {
+		PreparedStatement stmt = co.prepareStatement("SELECT * FROM users WHERE pseudo = ? AND password = ?");
+		stmt.setString(1, pseudo);
+		stmt.setString(2, password);
+		return stmt.executeQuery();
+	}
+	
+	
+}
